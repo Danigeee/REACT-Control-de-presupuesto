019518bba36373394126c02d48371d6134fb9297
Dany <daniav9924@gmail.com>
2022-12-20 13:24:20 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1> no me funciona el put auto complete </h1>
+    
+  )
+}
+
+export default App
